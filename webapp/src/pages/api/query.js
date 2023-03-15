bdfa1f9fbacd22ed1027cdbbcdf151c819a8ff65
ClemDev2000 <clement@cthiriet.com>
2023-03-15 14:55:32 +0100

    add streaming

---
+++ webapp/src/pages/api/query.js
@@ -0,0 +1,90 @@
+import { OpenAIStream } from "@/lib/OpenAIStream";
+import { PineconeClient } from "@pinecone-database/pinecone";
+
+const pinecone = new PineconeClient();
+
+if (!process.env.OPENAI_API_KEY) {
+  throw new Error("Missing env var from OpenAI");
+}
+
+export const config = {
+  runtime: "edge",
+};
+
+/**
+ *
+ * @param {import('next').NextApiRequest} req
+ * @returns
+ */
+const handler = async (req) => {
+  try {
+    const { namespace, query } = await req.json();
+
+    await pinecone.init({
+      environment: "us-east1-gcp",
+      apiKey: process.env.PINECONE_API_KEY,
+    });
+    const index = pinecone.Index(process.env.PINECONE_INDEX_NAME);
+
+    const res = await fetch("https://api.openai.com/v1/embeddings", {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${process.env.OPENAI_API_KEY ?? ""}`,
+      },
+      method: "POST",
+      body: JSON.stringify({
+        model: "text-embedding-ada-002",
+        input: query,
+      }),
+    });
+    let embedding = await res.json();
+    embedding = embedding.data[0].embedding;
+
+    const queryResponse = await index.query({
+      queryRequest: {
+        namespace,
+        topK: 4,
+        includeMetadata: true,
+        vector: embedding,
+      },
+    });
+
+    const texts = queryResponse.matches.map((result) => result.metadata.text);
+
+    const prompt = `Context: ${texts.join("")}
+    
+Question: ${query}
+
+Helpful Answer:`;
+
+    const messages = [
+      {
+        role: "system",
+        content:
+          "Use the pieces of context at the beginning of each query to answer the question at the end. If you don't know the answer, just say that you don't know, don't try to make up an answer.",
+      },
+      {
+        role: "user",
+        content: prompt,
+      },
+    ];
+
+    const payload = {
+      model: "gpt-3.5-turbo",
+      stream: true,
+      messages,
+    };
+
+    const stream = await OpenAIStream(payload);
+    return new Response(stream);
+  } catch (err) {
+    return new Response(
+      JSON.stringify({
+        error: err.message,
+      }),
+      { status: err.statusCode || 500 }
+    );
+  }
+};
+
+export default handler;
--- webapp/src/pages/index.js
+++ webapp/src/pages/index.js
@@ -1,117 +1,174 @@
+import { EmptyChat } from "@/components/EmptyChat";
 import { Header } from "@/components/Header";
-import { LoadingPulse } from "@/components/LoadingPulse";
-import { fetchPostJSON } from "@/lib/api-helpers";
-import { MagnifyingGlassIcon } from "@heroicons/react/20/solid";
-import { useEffect, useState } from "react";
+import { Spinner } from "@/components/Spinner";
+import { PaperAirplaneIcon } from "@heroicons/react/20/solid";
+import { useEffect, useRef, useState } from "react";
 import { useForm } from "react-hook-form";
 import { toast } from "react-hot-toast";
+import { v4 as uuidv4 } from "uuid";
 
-export default function Home() {
+function Interaction({ query, answer }) {
+  return (
+    <li className="py-6 border-t border-gray-200">
+      <p className="font-semibold font-opensans text-gray-900 text-base mb-3">
+        {query}
+      </p>
+      <p className="text-gray-600 font-medium font-opensans text-base leading-7">
+        {answer}
+      </p>
+    </li>
+  );
+}
+
+function Queries() {
   const {
     register,
     handleSubmit,
     reset,
+    watch,
     formState: { isSubmitting },
   } = useForm();
 
   const [responses, setResponses] = useState([]);
 
+  const [response, setResponse] = useState("");
+
+  const chatWindowRef = useRef(null);
+
+  const queryInput = watch("query");
+
+  useEffect(() => {
+    const storedResponses = localStorage.getItem("responses");
+    if (storedResponses) setResponses(JSON.parse(storedResponses));
+  }, []);
+
+  useEffect(() => {
+    if (chatWindowRef.current) {
+      // Scroll to the bottom of the chat window when the component updates
+      chatWindowRef.current.scrollTop = chatWindowRef.current?.scrollHeight;
+    }
+  }, [responses, response]);
+
   const ask = async ({ query }) => {
     try {
-      const res = await fetchPostJSON(process.env.NEXT_PUBLIC_API_URL, {
-        query: query,
+      const res = await fetch("/api/query", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          query,
+          namespace: process.env.NEXT_PUBLIC_PINECONE_INDEX_NAME,
+        }),
       });
-      setResponses([res, ...responses]);
-      localStorage.setItem("responses", JSON.stringify([res, ...responses]));
+
+      if (!res.ok) {
+        const data = await res.json();
+        throw new Error(data.error);
+      }
+
+      // This data is a ReadableStream
+      const data = res.body;
+      if (!data) {
+        return;
+      }
+
+      const reader = data.getReader();
+      const decoder = new TextDecoder();
+      let done = false;
+
+      let resDoc = "";
+
+      while (!done) {
+        const { value, done: doneReading } = await reader.read();
+        done = doneReading;
+        const chunkValue = decoder.decode(value);
+        setResponse((prev) => prev + chunkValue);
+        resDoc += chunkValue;
+      }
+
+      setResponse((prev) => prev.trim());
+      resDoc = resDoc.trim();
+
+      const newResponses = [
+        {
+          id: uuidv4(),
+          query,
+          answer: resDoc,
+        },
+        ...responses,
+      ];
+
+      setResponses(newResponses);
+
+      localStorage.setItem("responses", JSON.stringify(newResponses));
+
+      setResponse("");
+
       reset();
     } catch (err) {
       toast.error(err.message);
     }
   };
 
-  useEffect(() => {
-    const localResponses = localStorage.getItem("responses");
-    if (localResponses) {
-      setResponses(JSON.parse(localResponses));
-    }
-  }, []);
+  return (
+    <div className="flex flex-col overflow-y-auto px-8 flex-1">
+      {!responses.length && !response ? (
+        <EmptyChat />
+      ) : (
+        <ul
+          role="list"
+          ref={chatWindowRef}
+          className="pb-8 min-h-0 flex-1 overflow-y-auto flex flex-col-reverse max-w-4xl w-full mx-auto"
+        >
+          {response && <Interaction query={queryInput} answer={response} />}
 
+          {responses.map((query) => (
+            <Interaction
+              key={query.id}
+              query={query.query}
+              answer={query.answer}
+            />
+          ))}
+        </ul>
+      )}
+
+      <form onSubmit={handleSubmit(ask)} className="pb-12">
+        <div className="flex rounded-xl shadow-sm max-w-3xl mx-auto">
+          <div className="relative flex flex-grow items-stretch focus-within:z-10">
+            <input
+              type="text"
+              required
+              className="block w-full rounded-none rounded-l-xl border-gray-300 focus:border-sky-500 focus:ring-sky-500 py-3 px-4"
+              placeholder="Ask anything..."
+              {...register("query")}
+            />
+          </div>
+          <button
+            type="submit"
+            disabled={isSubmitting}
+            className="relative -ml-px inline-flex items-center space-x-2 rounded-r-xl border border-gray-300 bg-gray-50 px-6 py-2 text-sm font-medium text-gray-700 hover:bg-gray-100 focus:border-sky-500 focus:outline-none focus:ring-1 focus:ring-sky-500 disabled:opacity-50"
+          >
+            {isSubmitting ? (
+              <Spinner />
+            ) : (
+              <PaperAirplaneIcon
+                className="h-5 w-5 text-gray-400"
+                aria-hidden="true"
+              />
+            )}
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+}
+
+export default function Home() {
   return (
     <>
       <Header />
-      <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 mt-8 sm:mt-14 mb-8 w-full">
-        <div className="mx-auto max-w-3xl mb-8">
-          <div className="flex items-center justify-center pt-10">
-            <img src="/logo.svg" alt="Logo" width={200} />
-          </div>
-
-          <p className="mx-auto font-display mt-3 max-w-2xl text-center tracking-tight text-slate-500">
-            Get answers to your questions without browsing the documentation.
-          </p>
-
-          <form onSubmit={handleSubmit(ask)} className="mt-1 mb-5 pt-20">
-            <div className="flex rounded-full shadow font-display font-medium">
-              <div className="relative flex flex-grow items-stretch focus-within:z-10">
-                <input
-                  type="text"
-                  required
-                  className="block w-full rounded-none rounded-l-full py-3 px-4 border-gray-200 focus:border-orange-500 focus:ring-orange-500"
-                  placeholder="Ask your question..."
-                  {...register("query")}
-                />
-              </div>
-              <button
-                type="submit"
-                disabled={isSubmitting}
-                className="relative -ml-px inline-flex items-center space-x-2 rounded-r-full border border-gray-200 bg-gray-50 px-6 py-2 text-sm font-medium text-gray-700 hover:bg-gray-100 focus:border-orange-500 focus:outline-none focus:ring-1 focus:ring-orange-500 disabled:opacity-50"
-              >
-                <MagnifyingGlassIcon
-                  className="h-5 w-5 text-gray-400"
-                  aria-hidden="true"
-                />
-              </button>
-            </div>
-          </form>
-
-          {isSubmitting && <LoadingPulse className="pt-5" />}
-
-          <ul
-            role="list"
-            className="divide-y divide-gray-200 flex-1 pb-10 pt-5 mx-auto max-w-3xl"
-          >
-            {responses.map((query) => (
-              <li key={query.id} className="py-5">
-                <div className="mb-3 truncate text-ellipsis">
-                  <p className="font-semibold text-gray-900 text-base">
-                    {query.query}
-                  </p>
-                </div>
-                <p className="text-gray-700 text-base">{query.answer}</p>
-                <div className="space-x-2 mt-2">
-                  {query.sources && (
-                    <span className="text-gray-400 text-xs">Sources: </span>
-                  )}
-                  {query.sources?.map((source, index) => (
-                    <a
-                      href={source}
-                      target="_blank"
-                      rel="noopener noreferrer"
-                      className="text-xs py-1 px-2 bg-gray-100 text-gray-700 font-medium rounded"
-                    >
-                      {index + 1}
-                    </a>
-                  ))}
-                </div>
-                {/* {query.total_tokens && (
-                  <p className="text-gray-400 text-xs mt-2">
-                    {query.total_tokens} tokens processed
-                  </p>
-                )} */}
-              </li>
-            ))}
-          </ul>
-        </div>
-      </div>
+      <Queries />
     </>
   );
 }
